8f6120e72a370eb2664f988b4a98a5a2c0572399
bryceandy <bryceandy@rocketmail.com>
2022-03-07 17:51:21 +0300

    Match checkboxes with the color theme

--- resources/js/Jetstream/Checkbox.vue
+++ resources/js/Jetstream/Checkbox.vue
@@ -1,6 +1,6 @@
 <template>
     <input type="checkbox" :value="value" v-model="proxyChecked"
-           class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+           class="rounded border-gray-300 text-primary-800 shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-300 focus:ring-opacity-50">
 </template>
 
 <script>

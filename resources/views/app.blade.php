ced6b35e79ab389b8152b48025c77c646765af81
Yesaya R. Athuman <yesayaathuman@gmail.com>
2022-07-18 12:19:45 +0300

    Remove unused fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         
         <!-- Scripts -->
         @routes
